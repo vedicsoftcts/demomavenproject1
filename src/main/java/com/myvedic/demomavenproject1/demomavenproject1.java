0c5bd73d7d106ce3f5f40715151ff170e05e9516
Latif Vedic <latif@vedic.com>
2021-04-29 12:23:22 +0530

    2nd commit

--- src/main/java/com/myvedic/demomavenproject1/demomavenproject1.java
+++ src/main/java/com/myvedic/demomavenproject1/demomavenproject1.java
@@ -2,12 +2,11 @@ package com.myvedic.demomavenproject1;
 public class demomavenproject1 {
     public static void main(String[] args) {
         System.out.print("Hello, Maven Project");
-        /**
+        
         int a =20;
         int b =30;
         int c=a+b;
-        System.out.print("\na+b=" + c);
-        * */
+        System.out.print("\na+b=" + c);        
     }
     
 }
